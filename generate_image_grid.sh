aeea3a726e431fe9aad87955acbc0abfd83d2573
Jeremy Hu <huxingyi@msn.com>
2018-06-03 07:58:38 +0800

    Fix image grid

--- generate_image_grid.sh
+++ generate_image_grid.sh
@@ -2,4 +2,4 @@ convert \
 -page +0+0 models/anteater/anteater-render.png \
 -page +100+0 models/ant/ant-render.png \
 -page +200+0 models/panda/panda-render.png \
--background transparent -layers mosaic +repage free-dust3d-models.gif
+-background transparent -layers mosaic +repage free-dust3d-models.png
