b14b852c6f17d3205601aab1a273c4eb9e2394d0
Jeremy Hu <huxingyi@msn.com>
2018-06-21 17:09:09 +0800

    Add elephant render image to grid

--- generate_image_grid.sh
+++ generate_image_grid.sh
@@ -3,6 +3,7 @@ convert \
     -page +100+0 models/ant/ant-render.png \
     -page +200+0 models/panda/panda-render.png \
     -page +300+0 models/bottle/bottle-render.png \
+    -page +400+0 models/elephant/elephant-render.png \
 -background transparent -layers mosaic +repage free-dust3d-models.png
 
 convert free-dust3d-models.png \
